ecf8952641fdd3ac6baeaf31e43430f1e7267984
Maximiliano <maximilianokerps@gmail.com>
2024-01-07 15:07:38 -0300

    Images sections.

--- src/app/windows/pages/home/components/sections/sections.component.ts
+++ src/app/windows/pages/home/components/sections/sections.component.ts
@@ -1,4 +1,6 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { NgStyle } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
 
 // * Material.
 import { MatButtonModule } from '@angular/material/button';
@@ -7,9 +9,70 @@ import { MatIconModule } from '@angular/material/icon';
 @Component({
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [MatButtonModule, MatIconModule],
+  imports: [NgStyle, MatButtonModule, MatIconModule],
   selector: 'app-windows-home-sections',
   templateUrl: './sections.component.html',
   styleUrls: ['./sections.component.scss'],
 })
-export class SectionsComponent {}
+export class SectionsComponent {
+  public SECTIONS: ISection[] = [
+    {
+      title: 'APARTMENT',
+      description: 'Todos nuestros proyectos en construcción y finalizados.',
+      image: {
+        src: './assets/images/home/sections/aparment.tif',
+        alt: 'Apartamentos',
+      },
+    },
+    {
+      title: 'HOME',
+      description: 'Encontrá todas las variantes y modelos de casas en construcción de hormigón.',
+      image: {
+        src: 'assets/images/home/sections/home.tif',
+        alt: 'Casas',
+      },
+    },
+    {
+      title: 'INDUSTRY',
+      description: 'Nuestros proyectos industriales en construcción y finalizados.',
+      image: {
+        src: 'assets/images/home/sections/industry.tif',
+        alt: 'Industrias',
+      },
+    },
+    {
+      title: 'INVEST',
+      description: 'Invertí en nuestros proyectos y obtené una renta mensual.',
+      image: {
+        src: 'assets/images/home/sections/invest.tif',
+        alt: '',
+      },
+    },
+    // {
+    //   title: 'LOTE',
+    //   description: 'Desarrollos en diferentes localidades para que encuentres el que más se adapte a vos.',
+    //   image: {
+    //     src: '',
+    //     alt: '',
+    //     // src: 'assets/images/home/sections/'
+    //   },
+    // },
+  ];
+
+  private _sanitizer: DomSanitizer = inject(DomSanitizer);
+
+  public background(img: string): SafeStyle {
+    return this._sanitizer.bypassSecurityTrustStyle(`url(${img})`);
+  }
+}
+
+export interface ISection {
+  title: string;
+  description: string;
+  image: Img;
+}
+
+export interface Img {
+  src: string;
+  alt: string;
+}
