fb62bb87effc5a0e4fcc2e76999dd30a3922b42f
ecargnello <emiliano.cargnello@i2t-sa.com>
2024-01-07 16:35:34 -0300

    Implementacion de firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,9 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { Routes } from '@angular/router';
-import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient, withJsonpSupport } from '@angular/common/http';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { Routes, provideRouter } from '@angular/router';
 
 export const routes: Routes = [
   {
@@ -12,5 +13,23 @@ export const routes: Routes = [
 ];
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations(), provideHttpClient(withJsonpSupport())],
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    provideHttpClient(withJsonpSupport()),
+    importProvidersFrom(
+      provideFirebaseApp(() =>
+        initializeApp({
+          apiKey: 'AIzaSyDztbDJpEj62tXlc31Ybwvgpb41uZ33ksA',
+          authDomain: 'terraz-api.firebaseapp.com',
+          projectId: 'terraz-api',
+          storageBucket: 'terraz-api.appspot.com',
+          messagingSenderId: '1044337011728',
+          appId: '1:1044337011728:web:0fe6b19a018649d1386e95',
+          measurementId: 'G-HGSDZXCTKG',
+        })
+      )
+    ),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+  ],
 };
